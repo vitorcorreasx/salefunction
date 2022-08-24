4a601364244480323958341bd103a5a07f32820e
Vitor Correa <vitornbls2000@gmail.com>
2022-08-23 21:35:41 -0300

    api link update

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const scrapping = async () => {
   await page.goto(`https://www.pelando.com.br/busca?q=${msg}`);
 
   const response = await axios.get(
-    `https://www.pelando.com.br/api/graphql?operationName=SearchOffersQuery&variables=%7B%22query%22%3A%22${msg}%22%2C%22limit%22%3A18%2C%22sortBy%22%3A%22CREATED_AT%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%228f28ad48873aa383599f5649a90433ce9987d02612a9971ee903135046f6afd8%22%7D%7D`
-  );
+    `https://www.pelando.com.br/api/graphql?operationName=SearchOffersQuery&variables=%7B%22query%22%3A%22${msg}%22%2C%22limit%22%3A18%2C%22filters%22%3A%7B%22status%22%3A%22ACTIVE%22%7D%2C%22sortBy%22%3A%22CREATED_AT%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%228f28ad48873aa383599f5649a90433ce9987d02612a9971ee903135046f6afd8%22%7D%7D`  );
   const obj = response.data.data.public.searchOffers.edges;
   const list = [];
   for (key in obj) {
